19174046b2e509c5d273434c9b4f36fd31b90555
sanadsoman45 <sanadsoman871@gmail.com>
2024-04-20 12:52:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angularazuretest';
+
+  isLoggedin():boolean{
+    //check whether any user account is logged in or not.
+    return this.msalService.instance.getActiveAccount()!=null;
+  }
+
+
+  constructor(private msalService: MsalService) {
+    
+  }
+
+  //function to login:
+  login(){
+    this.msalService.loginPopup().subscribe({
+      next:(response:AuthenticationResult)=>{
+        //login attempt success? set account as active or will be null.
+        this.msalService.instance.setActiveAccount(response.account);
+      },
+      error:(error)=>{
+        // in case of error occured.
+        console.log(`Error has occured: ${error}`);
+      },
+      complete:()=>{
+        console.log("Success!");
+      }
+    })
+  }
+
+  logout(){
+    //logging out the user.
+    this.msalService.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,56 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  MSAL_INSTANCE,
+  MsalInterceptor,
+  MsalModule,
+  MsalService,
+} from '@azure/msal-angular';
+import {
+  BrowserCacheLocation,
+  IPublicClientApplication,
+  LogLevel,
+  PublicClientApplication,
+} from '@azure/msal-browser';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '',
+      authority:
+        '',
+      redirectUri: 'http://localhost:4200',
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+    },
+    system: {
+      allowNativeBroker: false, // Disables WAM Broker
+      loggerOptions: {
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false,
+      },
+    },
+  });
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MsalModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+    MsalService, //to login and logout
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
